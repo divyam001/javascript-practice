a492e0fd700b1b8d878651b8e4da1cfff61dcbdf
divyam001 <gupta.divyam001@gmail.com>
2021-09-12 12:58:15 +0530

    added oddevenn fn

---
+++ evenodd.js
@@ -0,0 +1,8 @@
+// if number is even it will return true ,if odd. false
+const number = prompt("Enter a number: ");
+if(number % 2 == 0) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
