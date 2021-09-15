6033677e988c4a3c94ef10e865ad943e24dadf61
Shubham Gupta <shubham.mailme@gmail.com>
2021-09-15 13:49:38 +0530

    Delete yourNumber.js

--- yourNumber.js
+++
@@ -1,8 +0,0 @@
-const number = prompt("enter your number");{
-if(number % 2 == 0){
-console.log("your number:" + number +  "is even");
-}
-else{
-console.log("your number:" + number +  "is odd");
- }
-}
