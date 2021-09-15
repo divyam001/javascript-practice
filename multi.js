fef2d28e07f3b2cfe56b4623289ffc61cb848e07
Shubham Gupta <shubham.mailme@gmail.com>
2021-09-15 13:50:40 +0530

    Update multi.js

--- multi.js
+++ multi.js
@@ -1,36 +1,23 @@
-
-
-function oddd(){
-var number;
-  number = 10;
-
-if(number % 2 == 0) {
-      return true;
-}
-else {
+function isNumberOdd(number){  
+  if(number % 2 === 0) {
+    return true;
+  } else {
      return false;
- }
+  }
 }
-
-function  evenn(){
-    var number;
-    number=33;
-    if (number % 2 == 0){
-        return( "number is even");
-
-    }
-    else {
-        return("number is odd");
-    }
+  
+function isNumberEvenn(number){
+  if (number % 2 == 0){
+    return "number is even";
+  } else {
+    return "number is odd";
+  }   
 }
-
-function detail(){
-    var number;
-    number= 100;
-    if (number % 2 == 0){
-        return("the number" + number +  "is even");
-    }
-    else{
-        return ("the number."  + number +  "..is odd");
-    }
+  
+function detail(number){
+  if (number % 2 === 0){
+      return `the number ${number} is even`;
+  } else {
+    return `the number ${number} is odd`;
+  }  
 }
