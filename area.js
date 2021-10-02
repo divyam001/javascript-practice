d000d1f13ed3a2289cc7285fa6c53b4838826446
divyam001 <gupta.divyam001@gmail.com>
2021-10-02 12:39:57 +0530

    added two fn

---
+++ area.js
@@ -0,0 +1,14 @@
+function AreaOfCircle(){
+    var area,Pie;
+    const number =prompt("enter radius: ");
+    pie=3.14;
+    area=3.14*number*number;
+    return area;
+}
+function AreaOfRectangle(){
+    var area,length,breadth;
+    const num1 =prompt("enter length: ");
+    const num2 =prompt("ENTER BREADTH: ");
+    area=num1*num2;
+    return area;
+}
